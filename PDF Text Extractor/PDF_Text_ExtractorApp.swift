9e72b47dae1934dfc2d99640f6b9c4ec1b109f8e
buildmase <mason.earl@icloud.com>
2025-10-20 14:08:46 -0600

    Initial Commit

---
+++ PDF Text Extractor/PDF_Text_ExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_Text_ExtractorApp.swift
+//  PDF Text Extractor
+//
+//  Created by Mason Earl on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PDF_Text_ExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
